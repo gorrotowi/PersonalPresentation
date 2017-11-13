8133b75bd1f150cf8da92ac24b7830c1a506897c
Sebastian Téllez <sebastian.tellez.o@gmail.com>
2017-11-12 20:57:40 -0600

    update profile view

--- app/src/main/java/com/gorrotowi/sebastiancv/MainActivity.kt
+++ app/src/main/java/com/gorrotowi/sebastiancv/MainActivity.kt
@@ -2,14 +2,26 @@ package com.gorrotowi.sebastiancv
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import org.jetbrains.anko.setContentView
+import android.text.Html
+import kotlinx.android.synthetic.main.content_sheet_dialog.*
+import org.jetbrains.anko.browse
+import org.jetbrains.anko.email
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val view = MainActivityUI().setContentView(this)
+        setContentView(R.layout.activity_main_layout)
 
+        txtProfilePosition.text = Html.fromHtml("Mobile Leader at <u>Chilangolabs</u>")
+
+        txtProfilePosition.setOnClickListener {
+            browse("https://chilangolabs.com/")
+        }
+
+        txtProfileEmail.setOnClickListener {
+            email(email = "sebastian@chilangolabs.com", subject = "Mobile Development Consult", text = "Hi Sebastian\n")
+        }
 
     }
 
