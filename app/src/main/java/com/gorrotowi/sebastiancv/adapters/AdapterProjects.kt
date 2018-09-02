f2d1dfe30c408451d13c01a8aec45a6b1d926bd3
Sebastian Tellez <sebastian.tellez.o@gmail.com>
2018-09-02 01:35:23 -0500

    update librarys

--- app/src/main/java/com/gorrotowi/sebastiancv/adapters/AdapterProjects.kt
+++ app/src/main/java/com/gorrotowi/sebastiancv/adapters/AdapterProjects.kt
@@ -14,19 +14,19 @@ import org.jetbrains.anko.browse
  */
 class AdapterProjects(val data: List<ItemProject>) : RecyclerView.Adapter<AdapterProjects.ViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ViewHolder =
-            ViewHolder(LayoutInflater.from(parent?.context).inflate(R.layout.item_project, parent, false))
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder =
+            ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_project, parent, false))
 
-    override fun onBindViewHolder(holder: ViewHolder?, position: Int) {
-        holder?.bindView(data[position])
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bindView(data[position])
     }
 
     override fun getItemCount(): Int = data.size
 
     class ViewHolder(v: View) : RecyclerView.ViewHolder(v) {
         fun bindView(item: ItemProject) {
-            itemView?.let {
-                with(it) {
+            itemView.let {view ->
+                with(view) {
                     txtItemProject.text = item.name
                     imgItemProject.setImageResource(item.img)
                     setOnClickListener {
